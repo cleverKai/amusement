48381bb86868b52a3cdffeeb789c6d03fbfdef61
cleverKai <951040436@qq.com>
2019-11-06 11:05:00 +0800

    add register

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -8,25 +8,28 @@
             <div class="inp"><input v-model="userName" type="text" placeholder="用户名" /></div>
             <div class="inp"><input v-model="userPwd" type="password" placeholder="密码" /></div>
             <div class="login" @click="handleLogin">登录</div>
-            <div id="bottom"><span id="registeredtxt">立即注册</span><span id="forgotpassword">忘记密码</span></div>
+            <div id="bottom"><span id="registeredtxt" >立即注册</span><span id="forgotpassword">忘记密码</span></div>
         </div>
         <div id="registered-content">
-            <div class="inp"><input  type="text" placeholder="请输入用户名" /></div>
-            <div class="inp"><input  type="password" placeholder="请输入密码" /></div>
-            <div class="inp"><input type="password" placeholder="请再次输入密码" /></div>
-            <div class="inp"><input type="text" placeholder="电子邮箱" /></div>
-            <div class="login">立即注册</div>
+            <div class="inp"><input v-model="userName" type="text" placeholder="请输入用户名" /></div>
+            <div class="inp"><input v-model="userPwd" type="password" placeholder="请输入密码" /></div>
+            <div class="inp"><input v-model="email" type="text" placeholder="电子邮箱" /> <button :disabled="disabled" @click="handleToVerify" class="verify">{{verifyInfo}}</button></div>
+            <div class="inp"><input v-model="verifyCode" type="password" placeholder="请输入验证码" /></div>
+            <div class="login" @click="handleToRegister">立即注册</div>
         </div>
     </div>
 </template>
-
 <script>
     export default {
         name: "login",
         data(){
             return{
                 userName: '',
-                userPwd: ''
+                userPwd: '',
+                email:'',
+                verifyInfo: '获取验证码',
+                disabled: false,
+                verifyCode:''
             }
         },
         mounted() {
@@ -61,8 +64,8 @@
                 }).then((res)=>{
                     let status = res.data.status;
                     if(status === 0){
-                        localStorage.setItem("username",this.userPwd);
-                        this.$store.commit('user/USER_NAME', {username:this.userName});
+                        localStorage.setItem('username',this.userName);
+                        this.$store.commit('user/USER_NAME',this.userName);
                         this.$alert('登录成功', '登录提示', {
                             confirmButtonText: '确定',
                             callback: action => {
@@ -75,6 +78,57 @@
                         });
                     }
                 })
+            },
+            handleToVerify(){
+                this.$axios.get('/api2/users/verify?email=' + this.email).then((res)=>{
+                    let status = res.data.status;
+                    if(status === 0){
+                        this.$alert('验证码已发送至邮箱，请注意查收', '消息提示', {
+                            confirmButtonText: '确定',
+                            callback: action => {
+                                this.countDown();
+                            }
+                        });
+
+                    }else {
+                        this.$alert(res.data.msg, '消息提示', {
+                            confirmButtonText: '确定',
+                        });
+                    }
+                });
+            },
+            countDown(){
+                let count =60;
+                this.disabled = true;
+                let timer = setInterval(()=>{
+                        this.verifyInfo = '剩余'+ count + '秒';
+                        count --;
+                        if(count == 0){
+                        this.verifyInfo = '再次发送';
+                        this.disabled = false
+                        clearInterval(timer)
+                    }
+
+                },1000)
+            },
+            handleToRegister(){
+                this.$axios.post('/api2/users/register', {
+                    username: this.userName,
+                    password: this.userPwd,
+                    email: this.email,
+                    verify: this.verifyCode
+                }).then((res) =>{
+                    let status = res.data.status;
+                    if( status === 0){
+                        this.$alert('用户注册成功，请登录', '消息提示', {
+                            confirmButtonText: '确定',
+                        });
+                    }else {
+                        this.$alert('用户注册失败,'+ res.data.msg, '消息提示', {
+                            confirmButtonText: '确定',
+                        });
+                    }
+                });
             }
         }
     }
@@ -82,6 +136,7 @@
 
 <style scoped lang="css">
     .form {
+        position: relative;
         position: absolute;
         top: 50%;
         left: 50%;
@@ -89,7 +144,7 @@
         margin-top: -210px;
         height: 420px;
         width: 340px;
-        font-size: 18px;
+        font-size: 14px;
         -webkit-box-shadow: 0px 0px 10px #A6A6A6;
         background: #fff;
     }
@@ -118,11 +173,20 @@
         margin-bottom: 30px;
         width: 200px;
     }
+    .inp .verify {
+        height: 30px;
+        font-size: 12px;
+        cursor: pointer;
+        color:#2a62ff;
+        font-weight: normal;
+        position: absolute;
+        right: 10px;
+    }
 
     .inp>input {
         text-align: center;
         height: 30px;
-        width: 200px;
+        width: 180px;
         margin: 0 auto;
         transition: all 0.3s ease-in-out;
     }
@@ -131,7 +195,7 @@
         border: 1px solid #A6A6A6;
         color: #a6a6a6;
         height: 30px;
-        width: 202px;
+        width: 182px;
         text-align: center;
         font-size: 13.333333px;
         margin-left: 70px;
